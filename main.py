c70e4e62ffe5f7b009a165665f4498e225c9d140
徐京伟 <2374539357@qq.com>
2022-02-26 14:56:27 +0800

    trained_model

--- main.py
+++ main.py
@@ -4,110 +4,311 @@ import math
 import json
 import sys
 import os
+import random
 from tqdm import tqdm
 from glob import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import json
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+USE_GPU = True
+if USE_GPU:
+    device = '/device:GPU:0'
+else:
+    device = '/cpu:0'
 
 BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, './utils'))
 
-import tensorflow as tf
-import numpy as np
-import tf_utils
-from transform_net import input_transform_layer, feature_transform_layer
-
-# Segmentation PointNet, input is BxNx3, output BxN*50
-
-def get_pointnet_seg(num_point, num_classes):
-    # input is B*N*3
-    # output B*N*50
-    input_points = tf.keras.Input(shape=(None, 3))
+with open("/Users/davidxu/dataset/PartAnnotation/metadata.json") as json_file:
+    metadata = json.load(json_file)
 
 
-    # get a transformation matrix
-    input_transform = input_transform_layer()(input_points)
-    # still B*N*3
-    point_cloud_transformed = tf.matmul(input_points, input_transform)
-    # B*N*3 => B*N*3*1
-    input_image = tf.expand_dims(point_cloud_transformed, -1)
+points_dir = "/Users/davidxu/dataset/PartAnnotation/{}/points".format(
+    metadata["Airplane"]["directory"]
+)
+labels_dir = "/Users/davidxu/dataset/PartAnnotation/{}/points_label".format(
+    metadata["Airplane"]["directory"]
+)
+LABELS = metadata["Airplane"]["lables"]
+COLORS = metadata["Airplane"]["colors"]
 
-    # B*N*3*1 => B*N*1*64
-    net = tf_utils.conv2d(input_image, 64, [1, 3],
-                          padding="valid",
-                          stride=[1, 1],
-                          bn=True)
+VAL_SPLIT = 0.2
+NUM_SAMPLE_POINTS = 1024
+BATCH_SIZE = 32
+EPOCHS = 60
+INITIAL_LR = 1e-3
 
-    # still B*N*1*64
-    net = tf_utils.conv2d(net, 64, [1, 1],
-                          padding="valid",
-                          stride=[1, 1],
-                          bn=True)
+point_clouds, test_point_clouds = [], []
+point_cloud_labels, all_labels = [], []
 
-    # get a transformation matrix
-    feature_transform = feature_transform_layer()(net)
+points_files = glob(os.path.join(points_dir, "*.pts"))
+for point_file in tqdm(points_files):
+    point_cloud = np.loadtxt(point_file)
+    if point_cloud.shape[0] < NUM_SAMPLE_POINTS:
+        continue
 
-    # B*N*1*64 => B*N*64
-    net_transformed = tf.matmul(tf.squeeze(net, axis=[2]), feature_transform)
+    # Get the file-id of the current point cloud for parsing its
+    # labels.
+    file_id = point_file.split("/")[-1].split(".")[0]
+    label_data, num_labels = {}, 0
+    for label in LABELS:
+        label_file = os.path.join(labels_dir, label, file_id + ".seg")
+        if os.path.exists(label_file):
+            label_data[label] = np.loadtxt(label_file).astype("float32")
+            num_labels = len(label_data[label])
 
-    # B*N*64 => B*N*1*64
-    point_feat = tf.expand_dims(net_transformed, [2])
+    # Point clouds having labels will be our training samples.
+    try:
+        label_map = ["none"] * num_labels
+        for label in LABELS:
+            for i, data in enumerate(label_data[label]):
+                label_map[i] = label if data == 1 else label_map[i]
+        label_data = [
+            LABELS.index(label) if label != "none" else len(LABELS)
+            for label in label_map
+        ]
+        # Apply one-hot encoding to the dense label representation.
+        label_data = tf.keras.utils.to_categorical(label_data, num_classes=len(LABELS) + 1)
 
-    # these multi-layer perceptrons maybe strive to find the important point
-    # or important feature in high dimension
-    # still B*N*1*64
-    net = tf_utils.conv2d(point_feat, 64, [1, 1],
-                          padding="valid",
-                          stride=[1, 1],
-                          bn=True)
+        point_clouds.append(point_cloud)
+        point_cloud_labels.append(label_data)
+        all_labels.append(label_map)
+    except KeyError:
+        test_point_clouds.append(point_cloud)
 
-    # B*N*1*64 => B*N*1*128
-    net = tf_utils.conv2d(net, 128, [1, 1],
-                          padding="valid",
-                          stride=[1, 1],
-                          bn=True)
 
-    # B*N*1*128 => B*N*1*1024
-    net = tf_utils.conv2d(net, 1024, [1, 1],
-                          padding="valid",
-                          stride=[1, 1],
-                          bn=True)
+for index in tqdm(range(len(point_clouds))):
+    current_point_cloud = point_clouds[index]
+    current_label_cloud = point_cloud_labels[index]
+    current_labels = all_labels[index]
+    num_points = len(current_point_cloud)
+    # Randomly sampling respective indices.
+    sampled_indices = random.sample(list(range(num_points)), NUM_SAMPLE_POINTS)
+    # Sampling points corresponding to sampled indices.
+    sampled_point_cloud = np.array([current_point_cloud[i] for i in sampled_indices])
+    # Sampling corresponding one-hot encoded labels.
+    sampled_label_cloud = np.array([current_label_cloud[i] for i in sampled_indices])
+    # Sampling corresponding labels for visualization.
+    sampled_labels = np.array([current_labels[i] for i in sampled_indices])
+    # Normalizing sampled point cloud.
+    norm_point_cloud = sampled_point_cloud - np.mean(sampled_point_cloud, axis=0)
+    norm_point_cloud /= np.max(np.linalg.norm(norm_point_cloud, axis=1))
+    point_clouds[index] = norm_point_cloud
+    point_cloud_labels[index] = sampled_label_cloud
+    all_labels[index] = sampled_labels
 
-    # extract global feature
-    # B*N*1*1024 => B*1*1*1024
-    global_feat = tf_utils.max_pool2d(net, [num_point, 1],
-                                      padding="valid")
-    # B*1*1*1024 => B*N*1*1024
-    global_feat_expand = tf.tile(global_feat, [1, num_point, 1, 1])
+def load_data(point_cloud_batch, label_cloud_batch):
+    point_cloud_batch.set_shape([NUM_SAMPLE_POINTS, 3])
+    label_cloud_batch.set_shape([NUM_SAMPLE_POINTS, len(LABELS) + 1])
+    return point_cloud_batch, label_cloud_batch
 
-    # B*N*1*64 and B*N*1*1024 => B*N*1*1088
-    concat_feat = tf.concat([point_feat, global_feat_expand],3)
 
-    # B*N*1*1088 => B*N*1*512
-    net = tf_utils.conv2d(concat_feat, 512, [1, 1],
-                          padding="valid", stride = [1,1],
-                          bn = True)
-    # B*N*1*512 => B*N*1*256
-    net = tf_utils.conv2d(net, 256, [1, 1],
-                          padding="valid", stride = [1,1],
-                          bn = True)
-    # B*N*1*256 => B*N*1*128
-    net = tf_utils.conv2d(net, 128, [1, 1],
-                          padding="valid", stride = [1,1],
-                          bn = True)
-    # still B*N*1*128
-    net = tf_utils.conv2d(net, 128, [1, 1],
-                          padding="valid", stride = [1,1],
-                          bn = True)
+def augment(point_cloud_batch, label_cloud_batch):
+    noise = tf.random.uniform(
+        tf.shape(label_cloud_batch), -0.005, 0.005, dtype=tf.float64
+    )
+    point_cloud_batch += noise[:, :, :3]
+    return point_cloud_batch, label_cloud_batch
 
-    # B*N*1*128 => B*N*1*50
-    # question: why this layer don't need training?
-    net = tf_utils.conv2d(net, num_classes, [1, 1],
-                          padding="valid",stride = [1, 1],
-                          activation_fn = None)
 
-    # B*N*1*50 => B*N*50
-    net = tf.squeeze(net, [2])
+def generate_dataset(point_clouds, label_clouds, is_training=True):
+    # from_tensor_slices return an iterative object
+    # it is a slice of the input
+    dataset = tf.data.Dataset.from_tensor_slices((point_clouds, label_clouds))
+    print(dataset)
+    # take batch
+    dataset = dataset.shuffle(BATCH_SIZE * 100) if is_training else dataset
+    print(dataset)
+    dataset = dataset.map(load_data, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    print(dataset)
+    # also changed to an iterative object
+    dataset = dataset.batch(batch_size=BATCH_SIZE)
+    print(dataset)
+    dataset = (
+        dataset.map(augment, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        if is_training
+        else dataset
+    )
+    print(dataset)
+    return dataset
 
-    return tf.keras.Model(input_points, net)
 
-a=get_pointnet_seg(3,4)
+split_index = int(len(point_clouds) * (1 - VAL_SPLIT))
+train_point_clouds = point_clouds[:split_index]
+print(np.array(train_point_clouds).shape)
+train_label_cloud = point_cloud_labels[:split_index]
+total_training_examples = len(train_point_clouds)
+
+val_point_clouds = point_clouds[split_index:]
+val_label_cloud = point_cloud_labels[split_index:]
+
+print("Num train point clouds:", len(train_point_clouds))
+print("Num train point cloud labels:", len(train_label_cloud))
+print("Num val point clouds:", len(val_point_clouds))
+print("Num val point cloud labels:", len(val_label_cloud))
+
+train_dataset = generate_dataset(train_point_clouds, train_label_cloud)
+val_dataset = generate_dataset(val_point_clouds, val_label_cloud, is_training=False)
+
+print("Train Dataset:", train_dataset)
+print("Validation Dataset:", val_dataset)
+
+
+def conv_block(x: tf.Tensor, filters: int, name: str) -> tf.Tensor:
+    x = layers.Conv1D(filters, kernel_size=1, padding="valid", name=f"{name}_conv")(x)
+    x = layers.BatchNormalization(momentum=0.0, name=f"{name}_batch_norm")(x)
+    return layers.Activation("relu", name=f"{name}_relu")(x)
+
+
+def mlp_block(x: tf.Tensor, filters: int, name: str) -> tf.Tensor:
+    x = layers.Dense(filters, name=f"{name}_dense")(x)
+    x = layers.BatchNormalization(momentum=0.0, name=f"{name}_batch_norm")(x)
+    return layers.Activation("relu", name=f"{name}_relu")(x)
+
+class OrthogonalRegularizer(keras.regularizers.Regularizer):
+    """Reference: https://keras.io/examples/vision/pointnet/#build-a-model"""
+
+    def __init__(self, num_features, l2reg=0.001):
+        self.num_features = num_features
+        self.l2reg = l2reg
+        self.identity = tf.eye(num_features)
+
+    def __call__(self, x):
+        x = tf.reshape(x, (-1, self.num_features, self.num_features))
+        xxt = tf.tensordot(x, x, axes=(2, 2))
+        xxt = tf.reshape(xxt, (-1, self.num_features, self.num_features))
+        return tf.reduce_sum(self.l2reg * tf.square(xxt - self.identity))
+
+def transformation_net(inputs: tf.Tensor, num_features: int, name: str) -> tf.Tensor:
+    """
+    Reference: https://keras.io/examples/vision/pointnet/#build-a-model.
+
+    The `filters` values come from the original paper:
+    https://arxiv.org/abs/1612.00593.
+    """
+    x = conv_block(inputs, filters=64, name=f"{name}_1")
+    x = conv_block(x, filters=128, name=f"{name}_2")
+    x = conv_block(x, filters=1024, name=f"{name}_3")
+    x = layers.GlobalMaxPooling1D()(x)
+    x = mlp_block(x, filters=512, name=f"{name}_1_1")
+    x = mlp_block(x, filters=256, name=f"{name}_2_1")
+    return layers.Dense(
+        num_features * num_features,
+        kernel_initializer="zeros",
+        bias_initializer=keras.initializers.Constant(np.eye(num_features).flatten()),
+        activity_regularizer=OrthogonalRegularizer(num_features),
+        name=f"{name}_final",
+    )(x)
+
+
+def transformation_block(inputs: tf.Tensor, num_features: int, name: str) -> tf.Tensor:
+    transformed_features = transformation_net(inputs, num_features, name=name)
+    transformed_features = layers.Reshape((num_features, num_features))(
+        transformed_features
+    )
+    return layers.Dot(axes=(2, 1), name=f"{name}_mm")([inputs, transformed_features])
+
+def get_shape_segmentation_model(num_points: int, num_classes: int) -> keras.Model:
+    input_points = keras.Input(shape=(None, 3))
+
+    # PointNet Classification Network.
+    transformed_inputs = transformation_block(
+        input_points, num_features=3, name="input_transformation_block"
+    )
+    features_64 = conv_block(transformed_inputs, filters=64, name="features_64")
+    features_128_1 = conv_block(features_64, filters=128, name="features_128_1")
+    features_128_2 = conv_block(features_128_1, filters=128, name="features_128_2")
+    transformed_features = transformation_block(
+        features_128_2, num_features=128, name="transformed_features"
+    )
+    features_512 = conv_block(transformed_features, filters=512, name="features_512")
+    features_2048 = conv_block(features_512, filters=2048, name="pre_maxpool_block")
+    global_features = layers.MaxPool1D(pool_size=num_points, name="global_features")(
+        features_2048
+    )
+    global_features = tf.tile(global_features, [1, num_points, 1])
+
+    # Segmentation head.
+    segmentation_input = layers.Concatenate(name="segmentation_input")(
+        [
+            features_64,
+            features_128_1,
+            features_128_2,
+            transformed_features,
+            features_512,
+            global_features,
+        ]
+    )
+    segmentation_features = conv_block(
+        segmentation_input, filters=128, name="segmentation_features"
+    )
+    outputs = layers.Conv1D(
+        num_classes, kernel_size=1, activation="softmax", name="segmentation_head"
+    )(segmentation_features)
+    return keras.Model(input_points, outputs)
+
+
+x, y = next(iter(train_dataset))
+
+num_points = x.shape[1]
+num_classes = y.shape[-1]
+
+with tf.device(device):
+    segmentation_model = get_shape_segmentation_model(num_points, num_classes)
+
+training_step_size = total_training_examples // BATCH_SIZE
+total_training_steps = training_step_size * EPOCHS
+print(f"Total training steps: {total_training_steps}.")
+
+lr_schedule = keras.optimizers.schedules.PiecewiseConstantDecay(
+    boundaries=[training_step_size * 15, training_step_size * 15],
+    values=[INITIAL_LR, INITIAL_LR * 0.5, INITIAL_LR * 0.25],
+)
+
+steps = tf.range(total_training_steps, dtype=tf.int32)
+lrs = [lr_schedule(step) for step in steps]
+
+# plt.plot(lrs)
+# plt.xlabel("Steps")
+# plt.ylabel("Learning Rate")
+# plt.show()
+
+
+def run_experiment(epochs):
+
+    segmentation_model = get_shape_segmentation_model(num_points, num_classes)
+    segmentation_model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
+        loss=keras.losses.CategoricalCrossentropy(),
+        metrics=["accuracy"],
+    )
+
+    # checkpoint_filepath = "/Users/davidxu/PycharmProjects/pointnet/tmp/checkpoint"
+    # checkpoint_callback = keras.callbacks.ModelCheckpoint(
+    #     checkpoint_filepath,
+    #     monitor="val_loss",
+    #     save_best_only=True,
+    #     save_weights_only=True,
+    # )
+
+    log_dir="logs/fit/"
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    history = segmentation_model.fit(
+        train_dataset,
+        validation_data=val_dataset,
+        epochs=epochs,
+        callbacks=[tensorboard_callback],
+    )
+
+    # segmentation_model.load_weights(checkpoint_filepath)
+    return segmentation_model, history
+
+
+segmentation_model, history = run_experiment(epochs=EPOCHS)
